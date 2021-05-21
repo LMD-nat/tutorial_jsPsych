f080940c568575d1a88d24dbc7f874fd5b690c4b
Nathalie <71288067+LMD-nat@users.noreply.github.com>
2021-05-20 22:33:54 -0400

    Create reading_task.js
    A very simple self-paces reading task for a tutorial, with some code switching and cognates thrown in for spice

---
+++ language/reading_task.js
@@ -0,0 +1,36 @@
+var dog = { // normal sentence
+      type: 'moving-window',
+      words: "The dog chased the car",
+    }
+    
+    var chien = { // code-switch on dog
+      type: 'moving-window',
+      words: "The chien chased the car"
+    }
+    
+    var voiture = { // code-switch on car
+      type: 'moving-window',
+      words: "The dog chased the voiture"
+    }
+    
+    var banana = { // normal sentence
+      type: 'moving-window',
+      words: "The monkey ate a banana"
+    }
+    
+    var banane = { // code-switch on banana
+      type: 'moving-window',
+      words: "The monkey ate a banane"
+    }  
+    
+    var singe = { // code-switch on monkey
+      type: 'moving-window',
+      words: "The singe ate a banana"
+    }
+
+var words = [dog, chien, voiture, banana, banane, singe];
+var experiment = jsPsych.randomization.shuffle(words);
+
+jsPsych.init({
+    timeline: experiment
+});
