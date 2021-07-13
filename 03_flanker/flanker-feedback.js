41ab2d0eb358ee3004af9de4f2e37994d2ff0549
Nathalie Germain <71288067+LMD-nat@users.noreply.github.com>
2021-07-13 12:03:20 -0400

    Update flanker-feedback.js

--- 03_flanker/flanker-feedback.js
+++ 03_flanker/flanker-feedback.js
@@ -1,6 +1,6 @@
 var timeline = [];
 
-var trial = {
+var trial_one = {
   type: 'html-keyboard-response',
   stimulus: '<<<<<',
   choices: ['z','m'],
@@ -11,6 +11,96 @@ var trial = {
   on_finish: function(data){
     // Score the response as correct or incorrect.
     if(jsPsych.pluginAPI.compareKeys(data.response, "z")){
+      data.correct = true;
+    } else {
+      data.correct = false; 
+    }
+  }
+}
+
+var trial_two = {
+  type: 'html-keyboard-response',
+  stimulus: '>>>>>',
+  choices: ['z','m'],
+  data: {
+    stimulus_type: 'congruent',
+    target_direction: 'right'
+  },
+  on_finish: function(data){
+    // Score the response as correct or incorrect.
+    if(jsPsych.pluginAPI.compareKeys(data.response, "m")){
+      data.correct = true;
+    } else {
+      data.correct = false; 
+    }
+  }
+}
+
+var trial_three = {
+  type: 'html-keyboard-response',
+  stimulus: '<<><<',
+  choices: ['z','m'],
+  data: {
+    stimulus_type: 'incongruent',
+    target_direction: 'right'
+  },
+  on_finish: function(data){
+    // Score the response as correct or incorrect.
+    if(jsPsych.pluginAPI.compareKeys(data.response, "m")){
+      data.correct = true;
+    } else {
+      data.correct = false; 
+    }
+  }
+}
+
+var trial_four = {
+  type: 'html-keyboard-response',
+  stimulus: '>><>>',
+  choices: ['z','m'],
+  data: {
+    stimulus_type: 'incongruent',
+    target_direction: 'left'
+  },
+  on_finish: function(data){
+    // Score the response as correct or incorrect.
+    if(jsPsych.pluginAPI.compareKeys(data.response, "z")){
+      data.correct = true;
+    } else {
+      data.correct = false; 
+    }
+  }
+}
+
+var trial_neutral_l = {
+  type: 'html-keyboard-response',
+  stimulus: '--<--',
+  choices: ['z','m'],
+  data: {
+    stimulus_type: 'incongruent',
+    target_direction: 'left'
+  },
+  on_finish: function(data){
+    // Score the response as correct or incorrect.
+    if(jsPsych.pluginAPI.compareKeys(data.response, "z")){
+      data.correct = true;
+    } else {
+      data.correct = false; 
+    }
+  }
+}
+
+var trial_neutral_r = {
+  type: 'html-keyboard-response',
+  stimulus: '-->--',
+  choices: ['z','m'],
+  data: {
+    stimulus_type: 'incongruent',
+    target_direction: 'left'
+  },
+  on_finish: function(data){
+    // Score the response as correct or incorrect.
+    if(jsPsych.pluginAPI.compareKeys(data.response, "m")){
       data.correct = true;
     } else {
       data.correct = false; 
@@ -34,4 +124,36 @@ var feedback = {
   }
 }
 
-timeline.push(trial, feedback);
+on = {
+  timeline: [trial_one, feedback],
+};
+
+tw = {
+  timeline: [trial_two, feedback],
+};
+
+th = {
+  timeline: [trial_three, feedback],
+};
+
+fo = {
+  timeline: [trial_four, feedback],
+};
+
+nl = {
+  timeline: [trial_neutral_l, feedback],
+};
+
+nr = {
+  timeline: [trial_neutral_r, feedback],
+};
+
+var flanker = [on, tw, th, fo, nr, nl, on, tw, th, fo, nr, nl, on, tw, th, fo, nr, nl, on, tw, th, fo, nr, nl, on, tw, th, fo, nr, nl];
+var experiment = jsPsych.randomization.shuffle(flanker);
+
+jsPsych.init({
+        timeline: experiment,
+        on_finish: function(data){
+			  document.body.innerHTML = '<p><center>Flanker data saved! You can now close this page.</center></p>'
+		}
+      });
